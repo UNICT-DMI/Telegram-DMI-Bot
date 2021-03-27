900f7858d661ac9acc054e22f23e0f574565b640
TendTo <casablancaernesto@gmail.com>
2021-03-27 13:08:57 +0100

    bump: lxml 4.6.2 -> 4.6.3 fix: absolute path to relative path

--- module/shared.py
+++ module/shared.py
@@ -12,7 +12,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 # config
-with open(r'D:\Programmazione\VSCode\Telegram-DMI-Bot\config\settings.yaml', 'r') as yaml_config:
+with open('config/settings.yaml', 'r') as yaml_config:
     config_map = yaml.load(yaml_config, Loader=yaml.SafeLoader)
 
 # Icons
