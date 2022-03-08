66d9dc2ad8818d8db1de0b4b63c2b8b68a842845
Daedalus9 <longo.luca94@hotmail.it>
2022-03-08 09:14:22 +0100

    refactor: added "drive_contribute.py" texts in "vars.py"

--- module/data/vars.py
+++ module/data/vars.py
@@ -5,3 +5,11 @@ DAY_SELECTION = "Seleziona la data della lezione che ti interessa."
 AULARIO_WARNING = "⚠️ Aulario non ancora pronto, riprova fra qualche minuto ⚠️"
 LESSON_SELECTION = "Quale lezione devi seguire?"
 NO_LESSON_WARNING = "Nessuna lezione programmata per questo giorno"
+
+
+""" drive_contribute.py """ 
+
+NO_USERNAME_WARNING = "Nessuno username"
+USE_TEXT = "USO: /drive_contribute [e-mail] [motivazione]\n\nESEMPIO: /drive_contribute mario.rossi@gmail.com Vorrei caricare i miei appunti di Fondamenti di Informatica"
+CONFIRM_ACCESS = "Hai ottenuto l'accesso in scrittura alla cartella Drive! \n\nPresto ti arriverà un'email di conferma per gli accessi in scrittura e potrai aggiungere appunti nella cartella mediante questo link https://cutt.ly/unict-dmi-drive"
+VALIDATION_ERROR = "Si é verificato un errore durante la validazione dell'email, riprova più tardi o verifica se hai già gli accessi in scrittura alla cartella mediante questo link https://cutt.ly/unict-dmi-drive"
