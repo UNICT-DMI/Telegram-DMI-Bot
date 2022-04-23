2e43cb71281a2dff5163f2ce7316f98d16db2937
LightDestory <apb231@gmail.com>
2022-04-23 02:06:58 +0200

    feat: multi_lang support

--- module/callback_handlers.py
+++ module/callback_handlers.py
@@ -1,16 +1,21 @@
 """Common query callback families"""
 import random
-from telegram import ParseMode, Update
+from typing import Optional
+
+from telegram import ParseMode, Update, CallbackQuery
 from telegram.ext import CallbackContext
+
+from module.data.vars import TEXT_IDS
 from module.shared import CUSicon, check_log, read_md
 # Needed to correctly run functions using globals()
 from module.commands.aulario import aulario
 from module.commands.esami import esami_button_anno, esami_button_insegnamento, esami_button_sessione
 from module.commands.lezioni import lezioni_button_anno, lezioni_button_giorno, lezioni_button_insegnamento
 from module.commands.help import help_back_to_menu, help_dip_cdl, help_rapp_menu, help_segr, help_ersu, help_misc, help_projects_acknowledgements
+from module.utils.multi_lang_utils import get_locale
 
 
-def submenu_handler(update: Update, context: CallbackContext):
+def submenu_handler(update: Update, context: CallbackContext) -> None:
     """Called by sm_.* callbacks.
     Opens the requested sub-menu, usually by editing the message and adding an InlineKeyboard
 
@@ -25,7 +30,22 @@ def submenu_handler(update: Update, context: CallbackContext):
     globals()[func_name](query, context, query.message.chat_id, query.message.message_id)
 
 
-def md_handler(update: Update, context: CallbackContext):
+def localization_handler(update: Update, context: CallbackContext) -> None:
+    """Called by any query that needs to show a localized text to the user
+
+    Args:
+        update: update event
+        context: context passed by the handler
+    """
+    locale: str = update.callback_query.from_user.language_code
+    query: Optional[CallbackQuery] = update.callback_query
+    data: str = query.data.replace("localization_", "")
+    message_text = get_locale(locale, TEXT_IDS[data])
+    check_log(update, data, is_query=True)
+    context.bot.editMessageText(text=message_text, chat_id=query.message.chat_id, message_id=query.message.message_id, parse_mode=ParseMode.MARKDOWN)
+
+
+def md_handler(update: Update, context: CallbackContext) -> None:
     """Called by any query that needs to show the contents of a markdown file to the user
 
     Args:
@@ -45,7 +65,7 @@ def md_handler(update: Update, context: CallbackContext):
     context.bot.editMessageText(text=message_text, chat_id=query.message.chat_id, message_id=query.message.message_id, parse_mode=ParseMode.MARKDOWN)
 
 
-def informative_callback(update: Update, context: CallbackContext):
+def informative_callback(update: Update, context: CallbackContext) -> None:
     """Called by any command that needs to show information to the user
 
     Args:
@@ -67,7 +87,7 @@ def informative_callback(update: Update, context: CallbackContext):
     context.bot.sendMessage(chat_id=update.message.chat_id, text=message_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=if_disable_preview)
 
 
-def none_handler(update: Update, context: CallbackContext):
+def none_handler(update: Update, context: CallbackContext) -> None:
     """Called when the user clicks an unactive button.
     Stops the spinning circle
 
@@ -78,7 +98,7 @@ def none_handler(update: Update, context: CallbackContext):
     update.callback_query.answer()
 
 
-def exit_handler(update: Update, context: CallbackContext):
+def exit_handler(update: Update, context: CallbackContext) -> None:
     """Called when the user wants to close a sub-menu.
     Removes the message from the user's chat
 
--- module/utils/multi_lang_utils.py
+++ module/utils/multi_lang_utils.py
@@ -1,3 +1,4 @@
+import os.path
 from glob import glob
 from os.path import basename
 from module.data.vars import TEXT_IDS
@@ -16,9 +17,16 @@ def get_locale(language_code: str, text_id: TEXT_IDS) -> str:
 
 
 def load_translations() -> None:
-    for language_file in glob(".\\data\\translations\\*.yaml"):
+    for language_file in glob(os.path.join("data", "translations", "*.yaml")):
         language_name: str = basename(language_file).split(".")[0]
         with open(language_file, 'r', encoding="UTF-8") as language_stream:
             translations[language_name] = yaml.load(language_stream, Loader=yaml.SafeLoader)
         language_stream.close()
+
+
+def get_regex_multi_lang(text_id: TEXT_IDS) -> str:
+    pattern: str = ""
+    for language in translations:
+        pattern = f'{pattern}{get_locale(language, text_id)}|'
+    return f'({pattern[:-1]})'
 
