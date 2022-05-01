d182bac301845f1c5f4c814b0a489f4901882156
Helias <stefanoborzi32@gmail.com>
2022-05-01 12:39:08 +0200

    chore: move logs into separate chat, clean settings

--- module/utils/send_utils.py
+++ module/utils/send_utils.py
@@ -6,14 +6,14 @@ from telegram.ext import CallbackContext
 # Modules
 from ..shared import config_map
 
-def send_log(update: Update, context: CallbackContext):
-    if(config_map['dev_group_chatid'] != 0 and update.message.chat_id == config_map['dev_group_chatid']):
+def send_log(update: Update, context: CallbackContext) -> None:
+    if config_map['dev_group_chatid'] != 0 and update.message.chat_id == config_map['dev_group_chatid']:
         context.bot.sendDocument(chat_id=config_map['dev_group_chatid'], document=open('logs/logs.txt', 'rb'))
 
-def send_chat_ids(update: Update, context: CallbackContext):
-    if(config_map['dev_group_chatid'] != 0 and update.message.chat_id == config_map['dev_group_chatid']):
+def send_chat_ids(update: Update, context: CallbackContext) -> None:
+    if config_map['dev_group_chatid'] != 0 and update.message.chat_id == config_map['dev_group_chatid']:
         context.bot.sendDocument(chat_id=config_map['dev_group_chatid'], document=open('logs/chatid.txt', 'rb'))
 
-def send_errors(update: Update, context: CallbackContext):
-    if(config_map['dev_group_chatid'] != 0 and update.message.chat_id == config_map['dev_group_chatid']):
+def send_errors(update: Update, context: CallbackContext) -> None:
+    if config_map['dev_group_chatid'] != 0 and update.message.chat_id == config_map['dev_group_chatid']:
         context.bot.sendDocument(chat_id=config_map['dev_group_chatid'], document=open('logs/errors.txt', 'rb'))
