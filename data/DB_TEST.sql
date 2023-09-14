58c7d51080d3afeaab92d1d0ea11a5aa9ebf1f46
Salvo Polizzi <plzslv03@studium.unict.it>
2023-09-14 15:58:09 +0200

    refactor: write sql code better

--- data/DB_TEST.sql
+++ data/DB_TEST.sql
@@ -2,31 +2,13 @@
 --(DROP TABLE IF EXISTS test_table);
 
 CREATE TABLE IF NOT EXISTS test_table (
-    id INT PRIMARY KEY,
-    string1 varchar(50) NOT NULL,
-    string2 varchar(50) NOT NULL
+    `id` INT PRIMARY KEY,
+    `string1` varchar(50) NOT NULL,
+    `string2` varchar(50) NOT NULL
 );
 
-INSERT INTO test_table (id, string1, string2) VALUES (
-    1, 
-    "test1", 
-    "TEST1"
-);
-
-INSERT INTO test_table (id, string1, string2) VALUES (
-    2, 
-    "test2", 
-    "TEST2"
-);
-
-INSERT INTO test_table (id, string1, string2) VALUES (
-    3, 
-    "test3", 
-    "TEST3"
-);
-
-INSERT INTO test_table (id, string1, string2) VALUES (
-    4, 
-    "test4", 
-    "TEST4"
-);
+INSERT INTO test_table (`id`, `string1`, `string2`) VALUES 
+(1, "test1", "TEST1"),
+(2, "test2", "TEST2"),
+(3, "test3", "TEST3"),
+(4, "test4", "TEST4");
